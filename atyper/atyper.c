7794a99ac6e4b9a3caf85412da93cf3c58664b7d
Sourav Datta <soura.jagat@gmail.com>
2019-09-29 19:40:27 +0530

    Add files via upload

---
+++ atyper/atyper.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXLENGTH 100
+
+const char keywords[][MAXLENGTH] = {
+    "begin",
+    "end",
+    "if",
+    "else",
+    "fi",
+    "then",
+    "case",
+    "esac",
+    "do",
+    "od",
+    "proc",
+    "int",
+    "real",
+    "long",
+    "string",
+    "char",
+    "local"
+};
+
+size_t keywords_count(void)
+{
+    return sizeof(keywords) / sizeof(keywords[0]);
+}
+
+int search_keyword(const char *str)
+{
+    int i;
+    for (i = 0; i < keywords_count(); i++) {
+        if (strcmp(keywords[i], str) == 0) return i;
+    }
+
+    return -1;
+}
+
+void replace_keyword(int index, char *token)
+{
+    const char *key;
+    size_t len = 0;
+    size_t i;
+
+    if (index < 0) {
+        printf("%s", token);
+    }
+    else {
+        key = keywords[index];
+    
+        while (*key) {
+            putchar(*key++ - ('a' - 'A'));
+        }
+    }
+}
+
+void search_replace_keyword(char *token)
+{
+    int kindex = search_keyword(token);
+    replace_keyword(kindex, token);
+}
+
+int main()
+{
+    char token[MAXLENGTH];
+    char *ptr = token;
+    int ch;
+    int kindex;
+
+    while ((ch = getchar()) != EOF) {
+        if (ptr - token >= MAXLENGTH - 1) {
+            ptr = token;
+            memset(token, 0, MAXLENGTH);
+        } 
+
+        if (!isalpha(ch)) {
+            *ptr = 0;
+            search_replace_keyword(token);    
+            ptr = token;
+            memset(token, 0, MAXLENGTH);
+            putchar(ch);
+        }
+        else {
+            *ptr++ = ch;
+        }
+    }
+
+    return 0;
+}
+
+
+
